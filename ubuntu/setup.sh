be2452fd49d499c9466b564bbf8ea89ffae8fa82
David Liu <david-khala@hotmail.com>
2025-06-20 13:45:39 +0000

    fix permission

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y -q qemu-system-x86 gvproxy
+sudo apt install -y -qq qemu-system-x86 gvproxy
 sudo mv /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-x86_64.real
-wget https://raw.githubusercontent.com/davidkhala/setup-podman/refs/heads/main/ubuntu/qemu-system-x86_64 -O /usr/bin/qemu-system-x86_64
+sudo wget https://raw.githubusercontent.com/davidkhala/setup-podman/refs/heads/main/ubuntu/qemu-system-x86_64 -O /usr/bin/qemu-system-x86_64
 sudo ln -s /usr/bin/gvproxy /usr/libexec/podman/gvproxy
