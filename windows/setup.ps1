5ef4ddd21cda954a2f84c911d3bab9954e7f421a
David Liu <david-khala@hotmail.com>
2025-06-22 16:07:50 +0000

    add to system path

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,2 +1,3 @@
 Invoke-WebRequest -Uri https://github.com/containers/podman/releases/download/v5.5.1/podman-5.5.1-setup.exe -OutFile podman-setup.exe
 Start-Process -FilePath ".\podman-setup.exe" -ArgumentList "/S" -Wait
+& "C:\Program Files\RedHat\Podman\podman.exe" machine init --user-mode-networking --now
