ef8923e5b1b882b5a63b331a5c210255961a4010
David Liu <david-khala@hotmail.com>
2025-06-22 13:53:22 +0000

    exclude macos

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Setup Podman context. It will
 
 - create and start a Podman machine
+
+## Limit
+
+`macos-latest` is not supported
+
+- macos-latest runners do not support nested virtualization, which is required for Podman's VM to function.
--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,5 +1,6 @@
-sudo apt update
+sudo apt -qq update
 sudo apt install -y -qq qemu-system-x86 gvproxy
 sudo mv /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-x86_64.real
 sudo wget https://raw.githubusercontent.com/davidkhala/setup-podman/refs/heads/main/ubuntu/qemu-system-x86_64 -O /usr/bin/qemu-system-x86_64
+sudo chmod +x /usr/bin/qemu-system-x86_64
 sudo ln -s /usr/bin/gvproxy /usr/libexec/podman/gvproxy
