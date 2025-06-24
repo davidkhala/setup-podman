58b355917f316aa369f1e04fddd1eef9b6c69245
David Liu <david-khala@hotmail.com>
2025-06-24 09:59:33 +0000

    see if ubuntu is prebuilt

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,5 +1,5 @@
 sudo apt -qq update
-sudo apt install -y -qq qemu-system-x86 gvproxy containernetworking-plugins
+sudo apt install -y -qq qemu-system-x86 gvproxy
 sudo mv /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-x86_64.real
 sudo wget https://raw.githubusercontent.com/davidkhala/setup-podman/refs/heads/main/ubuntu/qemu-system-x86_64 -O /usr/bin/qemu-system-x86_64
 sudo chmod +x /usr/bin/qemu-system-x86_64
