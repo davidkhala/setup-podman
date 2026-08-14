a4744b8d0d597a43450cf0bd4dda4541ed56e929
David Liu <david-khala@hotmail.com>
2026-08-14 21:23:16 +0800

    fix ci

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,8 @@
 sudo apt -qq update
-sudo apt install -y -qq qemu-system-x86 gvproxy containernetworking-plugins
+sudo apt install -y -qq qemu-system-x86 gvproxy containernetworking-plugins virtiofsd
 sudo mv /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-x86_64.real
 sudo wget https://raw.githubusercontent.com/davidkhala/setup-podman/refs/heads/main/ubuntu/qemu-system-x86_64 -O /usr/bin/qemu-system-x86_64
 sudo chmod +x /usr/bin/qemu-system-x86_64
+sudo mkdir -p /usr/libexec/podman
 sudo ln -s /usr/bin/gvproxy /usr/libexec/podman/gvproxy
+sudo ln -s /usr/libexec/virtiofsd /usr/local/bin/virtiofsd
